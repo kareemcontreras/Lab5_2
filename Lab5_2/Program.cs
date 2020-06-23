fa61039ecd4b2559b880bbe1613cca1b0e19e47e
kareemcontreras <66689752+kareemcontreras@users.noreply.github.com>
2020-06-23 09:19:07 -0500

    Add project files.

---
+++ Lab5_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab5_2
+{
+    class Program
+    {
+    public enum DaysoftheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            //Create a list to hold tasks
+            List<string> taskList = new List<string>();
+
+            //for each value in the Enum...let the user create a task.
+            foreach (DaysoftheWeek day in Enum.GetValues(typeof(DaysoftheWeek)))
+            {
+                Console.WriteLine("Enter a new task for" + day);
+                string task = day + " : " + Console.ReadLine();
+                taskList.Add(task);
+            }
+
+            //print all of the tasks.
+            foreach(string task in taskList)
+            {
+                Console.WriteLine(task);
+            }
+
+
+        }
+    }
+}
